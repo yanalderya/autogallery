495a820acea867ee7f1824f69fa04f551f565a9b
yanalderya <derya.yanal@gmail.com>
2018-05-07 15:30:50 +0300

    done

---
+++ app/src/main/java/com/example/dell/autogallery/interfaces/AutoOnItemClickListener.kt
@@ -0,0 +1,10 @@
+package com.example.dell.autogallery.interfaces
+
+import com.example.dell.autogallery.dto.AutoDTO
+
+/**
+ * Created by ${DERYA_YANAL} on 2.05.2018.
+ */
+interface AutoOnItemClickListener {
+    fun onItemClick(item:AutoDTO)
+}
